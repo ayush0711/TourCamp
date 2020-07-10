84f979c289483e5e7f6e71d801215b1e2626747d
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2020-07-10 22:35:33 +0530

    some changes and new ejs file

--- version1/app.js
+++ version1/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const bodyparser = require("body-parser");
 
 app.set("view engine", "ejs");
+app.use(bodyparser.urlencoded({ extended: true }));
 
 
 app.get("/", (req, res) => {
@@ -11,12 +13,23 @@ app.get("/", (req, res) => {
 
 app.get("/campgrounds", (req, res) => {
     const campgrounds = [
-        { name: "Kanha Agrawal", image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSQB6_iDI_SOiGu56Jpz6t3I-hiliG-95k5Zw&usqp=CAU" },
-        { name: "Akshay Dubey", image: "https://pawnacamp.com/wp-content/uploads/2018/01/Pawna-lake-camping-camp-F-new-768x512.jpg.webp" },
-        { name: "Harsh Jain", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80" }
+        { name: "Kanha Agrawal", image: "https://cdn.pixabay.com/photo/2019/10/03/11/14/camp-4522970__480.jpg " },
+        { name: "Akshay Dubey", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__480.jpg" },
+        { name: "Harsh Jain", image: "https://cdn.pixabay.com/photo/2018/12/24/22/19/camping-3893587__480.jpg" }
     ]
     res.render("campgrounds", { campgrounds: campgrounds });
 });
+
+app.post("/campgrounds", (req, res) => {
+    res.send("you hit the post");
+});
+
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("new.ejs");
+});
+
+
 
 app.listen(3000, () => {
     console.log("server running on port 3000");
