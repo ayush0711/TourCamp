741378aa258cc45ebff38b2840b29444384149fd
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2020-07-11 23:40:55 +0530

    some changes and add shoe.ejs file for more info

--- version1/app.js
+++ version1/app.js
@@ -1,20 +1,38 @@
 const express = require("express");
 const app = express();
-const bodyparser = require("body-parser"),
-    mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const mongoose = require("mongoose");
 
-mongoose.connect("mongod://localhost/Tour_Camp");
+mongoose.connect("mongodb://localhost:27017/TourCamp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.set("view engine", "ejs");
 app.use(bodyparser.urlencoded({ extended: true }));
-const campgrounds = [
-    { name: "Kanha Agrawal", image: "https://cdn.pixabay.com/photo/2019/10/03/11/14/camp-4522970__480.jpg " },
-    { name: "Akshay Dubey", image: "https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__480.jpg" },
-    { name: "Harsh Jain", image: "https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092__480.jpg" },
-    { name: "chirag garg", image: "https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402__480.jpg " },
-    { name: "radhika goyal", image: "https://cdn.pixabay.com/photo/2016/11/21/16/03/campfire-1846142__480.jpg" },
-    { name: "janvi goyal", image: "https://cdn.pixabay.com/photo/2016/02/09/16/35/night-1189929__480.jpg" }
-]
+
+//schema setup
+const campgroundschema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
+});
+
+const campground = mongoose.model("campground", campgroundschema);
+
+// campground.create({
+//     name: "Kanha Agrawal",
+//     image: "https://cdn.pixabay.com/photo/2019/10/03/11/14/camp-4522970__480.jpg ",
+//     description: "This is a huge granite hill, no bathrooms, no water"
+// }, function(err, Campground) {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Newly Created Campground");
+//         console.log(Campground);
+//     }
+// });
+
 
 
 app.get("/", (req, res) => {
@@ -23,21 +41,54 @@ app.get("/", (req, res) => {
 
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", { campgrounds: campgrounds });
+    //gt all camp on db
+    campground.find({}, function(err, allCampgrounds) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("index", { campgrounds: allCampgrounds });
+        }
+
+    });
+    //res.render("campgrounds", { campgrounds: campgrounds });
 });
 
 app.post("/campgrounds", (req, res) => {
     var name = req.body.name;
     const image = req.body.image;
-    const newCampground = { name: name, image: image }
-    campgrounds.push(newCampground);
-    res.redirect("/campgrounds");
+    const desc = req.body.description;
+    const newCampground = { name: name, image: image, description: desc }
+        //create a new camp for db
+    campground.create(newCampground, function(err, newlycreated) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
+    //campgrounds.push(newCampground);
+    //res.redirect("/campgrounds");
 });
 
 
 app.get("/campgrounds/new", (req, res) => {
     res.render("new.ejs");
 });
+//show more info about one campground
+app.get("/campgrounds/:id", (req, res) => {
+    //find camp with id
+    campground.findById(req.params.id, function(err, foundCampground) {
+        if (err) {
+            console.log(err);
+        } else {
+            //render show template with that camp
+            res.render("show", { campground: foundCampground });
+        }
+
+    });
+
+});
+
 
 
 
