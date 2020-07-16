f4bbec8c967bfb6dc33fc4937c89876ed180d593
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2020-07-17 00:20:16 +0530

    landing page animation and pricing feature

--- version1/models/campground.js
+++ version1/models/campground.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const campgroundschema = new mongoose.Schema({
     name: String,
+    price: String,
     image: String,
     description: String,
     author: {
