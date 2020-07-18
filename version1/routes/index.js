267f5d151ed3f07b7d1204a9e10c88fa276ef5c3
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2020-07-18 22:34:02 +0530

    install locus for admin role

--- version1/routes/index.js
+++ version1/routes/index.js
@@ -17,6 +17,9 @@ router.get("/register", function(req, res) {
 //handle sign up logic
 router.post("/register", function(req, res) {
     const newUser = new User({ username: req.body.username });
+    if (req.body.admincode === 'secretcode1234') {
+        newUser.isAdmin = true;
+    }
     User.register(newUser, req.body.password, function(err, user) {
         if (err) {
             console.log(err);
