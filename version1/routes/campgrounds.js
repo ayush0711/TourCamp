672cc49d20c8ff2bb5722ca85a8b2fc9a468c1c7
ayush0711 <ayush.mittal_cs17@gla.ac.in>
2020-07-22 23:27:45 +0530

    some changes for addition future like designing etc

--- version1/routes/campgrounds.js
+++ version1/routes/campgrounds.js
@@ -6,14 +6,30 @@ const middleware = require("../middleware");
 
 //INDEX - show all campgrounds
 router.get("/", (req, res) => {
-    // Get all campgrounds from DB
-    Campground.find({}, function(err, allCampgrounds) {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render("campgrounds/index", { campgrounds: allCampgrounds, page: 'campgrounds' });
-        }
-    });
+    var noMatch = null;
+    if (req.query.search) {
+        const regex = new RegExp(escapeRegex(req.query.search), 'gi');
+        Campground.find({ name: regex }, function(err, allCampgrounds) {
+            if (err) {
+                console.log(err);
+            } else {
+                if (allCampgrounds.length < 1) {
+                    noMatch = "No campgrounds match that query, please try again.";
+                }
+                res.render("campgrounds/index", { campgrounds: allCampgrounds, page: 'campgrounds', noMatch: noMatch });
+            }
+        });
+    } else {
+
+        // Get all campgrounds from DB
+        Campground.find({}, function(err, allCampgrounds) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("campgrounds/index", { campgrounds: allCampgrounds, page: 'campgrounds', noMatch: noMatch });
+            }
+        });
+    }
 });
 
 //CREATE - add new campground to DB
@@ -90,5 +106,10 @@ router.delete("/:id", middleware.checkCampgroundOwnership, (req, res) => {
         }
     });
 });
+
+
+function escapeRegex(text) {
+    return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
 
 module.exports = router;
